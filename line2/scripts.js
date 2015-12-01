efe9d1210e483e15a9353034baa8104acc94128c
Connor Patterson <cpatterson@shutterstock.com>
2015-11-30 21:08:11 -0500

    line chart

---
+++ line2/scripts.js
@@ -0,0 +1,90 @@
+$( "#weHaveAWinner" ).click(function() {
+  alert( "Money falling from the sky." );
+});
+
+var data = [{"JobTitle":"Monday I","Count":12},
+{"JobTitle":"Tuesday","Count":6},
+{"JobTitle":"Wednesday","Count":8},
+{"JobTitle":"Thursday","Count":9},
+{"JobTitle":"Friday","Count":6},
+{"JobTitle":"Saturday","Count":11},
+{"JobTitle":"Sunday","Count":9}];
+
+$( "#read" ).click(function() {
+	$("table").empty();
+	$( "table" ).append( "<th>Number of inches of rain per day</th>" );
+	$( "table" ).append( "<tr><td>" + "<u>Day</u>" + "</td><td>" + "<u>Inches</u>" + "</td></tr>" );
+  data.forEach( function(element, index, array) {
+		//if(index == 1){alert(element.JobTitle)};
+		if(index <= 17){
+			var string = "<tr><td>" + element.JobTitle + "</td><td class='barCount'>" + element.Count + "</td></tr>";
+			$( "table" ).append(string);
+
+		};
+	});
+});
+
+$( "#clear" ).click(function() {
+	$("table").empty();
+	$("#graph").empty();
+});
+  
+$( "#moveData" ).click(function() {
+	$("#graph").empty();
+  var multiplier = 400/12;
+  var index = 0;
+  var width = 400/data.length;
+  var margin = (width-7)/2;
+  data.forEach( function(element, index, array) {
+    $( "#graph" ).append( "<div class='dots' style='margin-left:"+margin+"px;margin-right:"+margin+"px;width:5px;height:5px;background-color:black;border:1px solid gray; margin-top:394px;'></div>" );
+  })
+  var intervalID2 = setInterval(function() {
+    var top = 400 - (data[index].Count * (multiplier-1));
+    $(".dots:nth-child("+(index+1)+")").animate({
+      "margin-top": top,
+    },{duration:1000,
+      start: function() {
+        console.log('start', index);
+        $("tr:nth-child("+(index+2)+") .barCount").css('font-weight', 'bold');
+      },
+      done: function(){
+        console.log('end', index);
+        $("tr:nth-child("+(index+0)+") .barCount").css('font-weight', '');
+      }
+    });
+    var dotStart = $(".dots:nth-child("+(index+1)+")");
+    var positionStart = dotStart.position();
+    var dotEnd = $(".dots:nth-child("+(index+2)+")");
+    var positionEnd = dotEnd.position();
+    console.log(positionStart);
+    console.log(positionEnd);
+    if(index < 6){
+      createLine(positionStart.left, positionStart.top, positionEnd.left, positionEnd.top);
+    }
+    console.log(index, data.length);
+    if(index == data.length-1) {
+      console.log("cleared");
+			clearInterval(intervalID2);
+		}
+    index++;
+  }, 800);	
+});
+
+//http://www.monkeyandcrow.com/blog/drawing_lines_with_css3/
+function createLine(x1,y1, x2,y2){
+  var length = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+  console.log(length);
+  var angle  = Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+  console.log(angle);
+  var transform = 'rotate('+angle+'deg)';
+  var line = $('<div>')
+    .appendTo('#graph')
+    .addClass('line')
+    .css({
+      'position': 'absolute',
+      'transform': transform
+    })
+    .width(length)
+    .offset({left: x1, top: y1});
+  return line;
+};
