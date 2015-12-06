98d6a05a396e0ee1d2f9f82eb2e0dc7f41a993de
mmackre <mmackre@ncsu.edu>
2015-12-06 15:37:00 -0500

    Update scripts.js

--- line/scripts.js
+++ line/scripts.js
@@ -2,21 +2,21 @@ $( "#weHaveAWinner" ).click(function() {
   alert( "Money falling from the sky." );
 });
 
-var data = [{"JobTitle":"Programmer I","Count":196},
-{"JobTitle":"Civil Engineer","Count":216},
-{"JobTitle":"Software Engineer IV","Count":400},
-{"JobTitle":"Web Designer III","Count":132},
-{"JobTitle":"Human Resources Manager","Count":88},
-{"JobTitle":"Help Desk Technician","Count":252}];
+var data = [{"Key":"Programmer I","Value":196},
+{"Key":"Civil Engineer","Value":216},
+{"Key":"Software Engineer IV","Value":400},
+{"Key":"Web Designer III","Value":132},
+{"Key":"Human Resources Manager","Value":88},
+{"Key":"Help Desk Technician","Value":252}];
 
 $( "#read" ).click(function() {
 	$("table").empty();
 	$( "table" ).append( "<th>Number of workers in certian job fields</th>" );
-	$( "table" ).append( "<tr><td>" + "<u>Job Title</u>" + "</td><td>" + "<u>Count</u>" + "</td></tr>" );
+	$( "table" ).append( "<tr><td>" + "<u>Job Title</u>" + "</td><td>" + "<u>Value</u>" + "</td></tr>" );
   data.forEach( function(element, index, array) {
-		//if(index == 1){alert(element.JobTitle)};
+		//if(index == 1){alert(element.Key)};
 		if(index <= 17){
-			var string = "<tr><td>" + element.JobTitle + "</td><td class='barCount'>" + element.Count + "</td></tr>";
+			var string = "<tr><td>" + element.Key + "</td><td class='barValue'>" + element.Value + "</td></tr>";
 			$( "table" ).append(string);
 
 		}
@@ -30,18 +30,18 @@ $( "#clear" ).click(function() {
 });
 
 // Enter data for x-axis of bar graph
-var data2 = [{"JobTitle2":"Prog I"},
-             {"JobTitle2":"CE"},
-             {"JobTitle2":"SE IV"},
-             {"JobTitle2":"WD III"},
-             {"JobTitle2":"HRM"},
-             {"JobTitle2":"HDT"}];
+var data2 = [{"Key2":"Prog I"},
+             {"Key2":"CE"},
+             {"Key2":"SE IV"},
+             {"Key2":"WD III"},
+             {"Key2":"HRM"},
+             {"Key2":"HDT"}];
 $( "#moveData" ).click(function() {
   $("#graph").empty();
   var index = 0;
-  var max = data[0].Count;
+  var max = data[0].Value;
   for (var i = 0; i < data2.length; i++){
-    if (data[i].Count > max) max = data[i].Count;
+    if (data[i].Value > max) max = data[i].Value;
   }
   var scale = 400/max;
   $( "#graph" ).append( "<div id='yaxis' style='width: 20px; height: 400px'></div>" );
@@ -52,15 +52,15 @@ $( "#moveData" ).click(function() {
   var width;
   var intervalID2 = setInterval(function() {
     width = ((parseInt($("#graph").css("width"), 10) - 20)/data2.length) - 2;
-    var height = data[index].Count * scale;
+    var height = data[index].Value * scale;
     var top = 400 - height;
     $( "#ghost" ).append( "<div class='lines' style='width: 2 px; margin-top: -180px;  background-color:#BDBDBD; border:1px solid black'></div>" );  
    
-    $( "#graph" ).append( "<div class='bar' style='width:"+width+"px; margin-top:"+top+"px; background-color:#BDBDBD; border:1px solid black'>" + data2[index].JobTitle2 + "</div>" );
+    $( "#graph" ).append( "<div class='bar' style='width:"+width+"px; margin-top:"+top+"px; background-color:#BDBDBD; border:1px solid black'>" + data2[index].Key2 + "</div>" );
     $(".bar:nth-child("+(index+2)+")").animate({ height: "" + height + "px"}, 800);
-     $("tr:nth-child("+(index+2)+") .barCount").css('font-weight', 'bold');
+     $("tr:nth-child("+(index+2)+") .barValue").css('font-weight', 'bold');
     
-     $("tr:nth-child("+(index+1)+") .barCount").css('font-weight', '');
+     $("tr:nth-child("+(index+1)+") .barValue").css('font-weight', '');
     if(index >= data.length) {
         console.log("exiting loop");
        
