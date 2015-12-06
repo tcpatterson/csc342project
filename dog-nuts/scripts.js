b6b9c46500e0ca86c2f36d524d8f9e60bcfe67df
mmackre <mmackre@ncsu.edu>
2015-12-06 18:16:59 -0500

    Update scripts.js

--- dog-nuts/scripts.js
+++ dog-nuts/scripts.js
@@ -2,18 +2,16 @@ $( "#weHaveAWinner" ).click(function() {
   alert( "Money falling from the sky." );
 });
 
-var data = [{"Key":"Monday","Value":12},
-{"Key":"Tuesday","Value":6},
-{"Key":"Wednesday","Value":8},
-{"Key":"Thursday","Value":9},
-{"Key":"Friday","Value":6},
-{"Key":"Saturday","Value":11},
-{"Key":"Sunday","Value":9}];
+var data = [{"Key":"A's","Value":20},
+{"Key":"B's","Value":24},
+{"Key":"C's","Value":37},
+{"Key":"D's","Value":13},
+{"Key":"F's","Value":8}];
 
 $( "#read" ).click(function() {
 	$("table").empty();
-	$( "table" ).append( "<th>Number of inches of rain per day</th>" );
-	$( "table" ).append( "<tr><td>" + "<u>Day</u>" + "</td><td>" + "<u>Inches</u>" + "</td></tr>" );
+	$( "table" ).append( "<th>Grade Distribution of Operating Systems</th>" );
+	$( "table" ).append( "<tr><td>" + "<u>Day</u>" + "</td><td>" + "<u>Number of Grades</u>" + "</td></tr>" );
   data.forEach( function(element, index, array) {
 		//if(index == 1){alert(element.Key)};
 		if(index <= 17){
