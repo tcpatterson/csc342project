50e5b8f67b85a932ccdbc7008d89ca60b8a066f4
jmschae3 <jmschae3@ncsu.edu>
2015-12-06 16:44:32 -0500

    key value, large data, all that

--- line/scripts.js
+++ line/scripts.js
@@ -2,30 +2,30 @@ $( "#weHaveAWinner" ).click(function() {
   alert( "Money falling from the sky." );
 });
 
-var data = [{"JobTitle":"Programmer 1","Count":196},
-{"JobTitle":"Civil Engineer","Count":216},
-{"JobTitle":"Software Engineer 4","Count":400},
-{"JobTitle":"Web Designer 3","Count":132},
-{"JobTitle":"Human Resources Manager","Count":88},
-{"JobTitle":"Help Desk Technician","Count":252},
-{"JobTitle":"Programmer 1","Count":196},
-{"JobTitle":"Civil Engineer","Count":216},
-{"JobTitle":"Software Engineer 4","Count":400},
-{"JobTitle":"Web Designer 3","Count":132},
-{"JobTitle":"Human Resources Manager","Count":88},
-{"JobTitle":"Help Desk Technician","Count":252},
-{"JobTitle":"Programmer 1","Count":196},
-{"JobTitle":"Civil Engineer","Count":216},
-{"JobTitle":"Software Engineer IV","Count":400},
-{"JobTitle":"Web Designer 3","Count":132},
-{"JobTitle":"Human Resources Manager","Count":88},
-{"JobTitle":"Help Desk Technician","Count":252},
-{"JobTitle":"Programmer 1","Count":196},
-{"JobTitle":"Civil Engineer","Count":216},
-{"JobTitle":"Software Engineer 4","Count":400},
-{"JobTitle":"Web Designer 3","Count":132},
-{"JobTitle":"Human Resources Manager","Count":88},
-{"JobTitle":"Help Desk Technician","Count":252}            
+var data = [{"Key":"Programmer 1","Value":196},
+{"Key":"Civil Engineer","Value":216},
+{"Key":"Software Engineer 4","Value":400},
+{"Key":"Web Designer 3","Value":132},
+{"Key":"Human Resources Manager","Value":88},
+{"Key":"Help Desk Technician","Value":252},
+{"Key":"Programmer 1","Value":196},
+{"Key":"Civil Engineer","Value":216},
+{"Key":"Software Engineer 4","Value":400},
+{"Key":"Web Designer 3","Value":132},
+{"Key":"Human Resources Manager","Value":88},
+{"Key":"Help Desk Technician","Value":252},
+{"Key":"Programmer 1","Value":196},
+{"Key":"Civil Engineer","Value":216},
+{"Key":"Software Engineer IV","Value":400},
+{"Key":"Web Designer 3","Value":132},
+{"Key":"Human Resources Manager","Value":88},
+{"Key":"Help Desk Technician","Value":252},
+{"Key":"Programmer 1","Value":196},
+{"Key":"Civil Engineer","Value":216},
+{"Key":"Software Engineer 4","Value":400},
+{"Key":"Web Designer 3","Value":132},
+{"Key":"Human Resources Manager","Value":88},
+{"Key":"Help Desk Technician","Value":252}            
            ];
 
 $( "#read" ).click(function() {
@@ -33,9 +33,9 @@ $( "#read" ).click(function() {
 	$( "table" ).append( "<th>Number of workers in certian job fields</th>" );
 	$( "table" ).append( "<tr><td>" + "<u>Job Title</u>" + "</td><td>" + "<u>Count</u>" + "</td></tr>" );
   data.forEach( function(element, index, array) {
-		//if(index == 1){alert(element.JobTitle)};
+		//if(index == 1){alert(element.Key)};
 		if(index <= 17){
-			var string = "<tr><td>" + element.JobTitle + "</td><td class='barCount'>" + element.Count + "</td></tr>";
+			var string = "<tr><td>" + element.Key + "</td><td class='barCount'>" + element.Value + "</td></tr>";
 			$( "table" ).append(string);
 
 		}
@@ -51,9 +51,9 @@ $( "#clear" ).click(function() {
 $( "#moveData" ).click(function() {
   $("#graph").empty();
   var index = 0;
-  var max = data[0].Count;
+  var max = data[0].Value;
   for (var i = 0; i < data.length; i++){
-    if (data[i].Count > max) max = data[i].Count;
+    if (data[i].Value > max) max = data[i].Value;
   }
   var scale = 400/max;
   $( "#graph" ).append( "<div id='yaxis' style='width: 20px; height: 400px'></div>" );
@@ -64,10 +64,10 @@ $( "#moveData" ).click(function() {
   var width;
   var intervalID2 = setInterval(function() {
     width = ((parseInt($("#graph").css("width"), 10) - 20)/data.length) - 2;
-    var height = data[index].Count * scale;
+    var height = data[index].Value * scale;
     var top = 400 - height;
     var abbreviation = "";
-    var split = data[index].JobTitle.split(" ");
+    var split = data[index].Key.split(" ");
     for (var q = 0; q < split.length; q++) abbreviation += split[q].charAt(0);
     $( "#ghost" ).append( "<div class='lines' style='width: 2 px; margin-top: -180px;  background-color:#BDBDBD; border:1px solid black'></div>" );  
    
