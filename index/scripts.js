aab305db0d1b64c5ed8124730292938bb5b32777
Connor Patterson <cpatterson@shutterstock.com>
2015-12-10 12:06:46 -0500

    some finalizing touches

--- index/scripts.js
+++ index/scripts.js
@@ -1,13 +1,12 @@
-$( "#weHaveAWinner" ).on('click', toggleSidebar);
-$( "#read" ).on('click', readLineData);
-$( "#clear" ).on('click', clearLine);
-$( "#genChart" ).on('click', genLineChart);
-$( "#changeWidth" ).on('click', resizeLine);
+$( "#toggler" ).on('click', toggleSidebar);
+$( "#read" ).on('click', readBarData);
+$( "#clear" ).on('click', clearBar);
+$( "#genChart" ).on('click', genBarChart);
+$( "#changeWidth" ).on('click', resizeBar);
 $("#bar").click(function(){
+  $("#currentChartTitle").text("Bar Chart")
   $('#changeWidthSpan').removeClass('hide');
-  $( "#read" ).unbind();
-  $( "#clear" ).unbind();
-  $( "#genChart" ).unbind();
+  unbindAll();
   $( "#changeWidth" ).unbind();
   $( "#changeWidth" ).on('click', resizeBar);
   $( "#read" ).on('click', readBarData);
@@ -15,14 +14,12 @@ $("#bar").click(function(){
   $( "#genChart" ).on('click', genBarChart);
   toggleSidebar();
   clearBar();
-  $("#graph").animate({'width' : 400}, 1000, function(){});
-  $(".charts").animate({'width' : 860}, 1000, function(){});
+  undoResize();
 })
 $("#line").click(function(){
+  $("#currentChartTitle").text("Line Chart")
   $('#changeWidthSpan').removeClass('hide');
-  $( "#read" ).unbind();
-  $( "#clear" ).unbind();
-  $( "#genChart" ).unbind();
+  unbindAll();
   $( "#changeWidth" ).unbind();
   $( "#changeWidth" ).on('click', resizeLine);
   $( "#read" ).on('click', readLineData);
@@ -30,29 +27,52 @@ $("#line").click(function(){
   $( "#genChart" ).on('click', genLineChart);
   toggleSidebar();
   clearBar();
-  $("#graph").animate({'width' : 400}, 1000, function(){});
-  $(".charts").animate({'width' : 860}, 1000, function(){});
+  undoResize();
 })
 $("#pie").click(function(){
+  $("#currentChartTitle").text("Pie Chart")
   $('#changeWidthSpan').addClass('hide');
-  $( "#read" ).unbind();
-  $( "#clear" ).unbind();
-  $( "#genChart" ).unbind();
+  unbindAll();
   $( "#read" ).on('click', readPieData);
   $( "#clear" ).on('click', clearPie);
   $( "#genChart" ).on('click', genPieChart);
   toggleSidebar();
   clearBar();
+  undoResize();
+})
+$("#donut").click(function(){
+  $("#currentChartTitle").text("Donut Chart")
+  $('#changeWidthSpan').addClass('hide');
+  unbindAll();
+  $( "#read" ).on('click', readDonutData);
+  $( "#clear" ).on('click', clearDonut);
+  $( "#genChart" ).on('click', genDonutChart);
+  toggleSidebar();
+  clearBar();
+  undoResize();
+})
+
+function undoResize() {
   $("#graph").animate({'width' : 400}, 1000, function(){});
   $(".charts").animate({'width' : 860}, 1000, function(){});
-})
+}
+
+function unbindAll() {
+  $( "#read" ).unbind();
+  $( "#clear" ).unbind();
+  $( "#genChart" ).unbind();
+}
 
 function toggleSidebar() {
   var currentLeft = $("#sidebar").css( "left" );
   if(currentLeft == "-150px") {
     $("#sidebar").animate({ left: "0px"}, 500);
+    $("#toggler").removeClass("pushRight");
+    $("#toggler").addClass("pushLeft");
   } else {
     $("#sidebar").animate({ left: "-150px"}, 500);
+    $("#toggler").removeClass("pushLeft");
+    $("#toggler").addClass("pushRight");
   }
 }
 
@@ -71,15 +91,7 @@ var barData = [{"Key":"Programmer 1","Value":196},
 {"Key":"Programmer 1","Value":196},
 {"Key":"Civil Engineer","Value":216},
 {"Key":"Software Engineer IV","Value":400},
-{"Key":"Web Designer 3","Value":132},
-{"Key":"Human Resources Manager","Value":88},
-{"Key":"Help Desk Technician","Value":252},
-{"Key":"Programmer 1","Value":196},
-{"Key":"Civil Engineer","Value":216},
-{"Key":"Software Engineer 4","Value":400},
-{"Key":"Web Designer 3","Value":132},
-{"Key":"Human Resources Manager","Value":88},
-{"Key":"Help Desk Technician","Value":252}];
+{"Key":"Web Designer 3","Value":132}];
 
 
 var lineData = [{"Key":"Monday","Value":12},
@@ -98,23 +110,42 @@ var pieData = [{"Key":"Monday","Value":12},
 {"Key":"Saturday","Value":11},
 {"Key":"Sunday","Value":9}];
 
+var donutData = [{"Key":"A's","Value":20},
+{"Key":"B's","Value":24},
+{"Key":"C's","Value":37},
+{"Key":"D's","Value":13},
+{"Key":"F's","Value":8}];
+
 var index = 0;
 var ready;
-var total = 0;
+var pieTotal = 0;
 pieData.forEach(function(element, index, array) {
-  total = total + element.Value;
+  pieTotal = pieTotal + element.Value;
 });
-var percentages = [];
+var piePercentages = [];
 pieData.forEach(function(element, index, array) {
-  percentages.push(element.Value/total);
+  piePercentages.push(element.Value/pieTotal);
 });
-var angles = [];
+var pieAngles = [];
 pieData.forEach(function(element, index, array) {
-  angles.push(percentages[index]*360);
+  pieAngles.push(piePercentages[index]*360);
 });
 var i;
 var lastAngle = 0;
 var timeoutID;
+
+var donutTotal = 0;
+donutData.forEach(function(element, index, array) {
+  donutTotal = donutTotal + element.Value;
+});
+var donutPercentages = [];
+donutData.forEach(function(element, index, array) {
+  donutPercentages.push(element.Value/donutTotal);
+});
+var donutAngles = [];
+donutData.forEach(function(element, index, array) {
+  donutAngles.push(donutPercentages[index]*360);
+});
 
 function readBarData() {
   var data = barData;
@@ -135,12 +166,13 @@ function clearBar() {
   $("table").empty();
   $("#graph").empty();
   $("#ghost").empty();
+  undoResize();
 }
 
 function resizeLine() {
   var newWidth = parseInt($("#width").val(), 10);
   console.log($("#width").val());
-  if (newWidth > 10 && newWidth < 10000) {
+  if (newWidth > 10 && newWidth <= 10000) {
     $("#graph").empty();
     $("#graph").css("width", newWidth + "px");
     $(".charts").css("width", (newWidth + 460) + "px");
@@ -151,7 +183,7 @@ function resizeBar() {
   var data = barData;
   var newWidth = parseInt($("#width").val(), 10);
   console.log($("#width").val());
-  if (newWidth > 10 && newWidth < 10000) {
+  if (newWidth > 10 && newWidth <= 10000) {
     var changeBar =  {'width' : (((newWidth - 20)/data.length) - 2)};
     var changeGraph =  {'width' : newWidth};
     var changeCharts =  {'width' : (newWidth + 460)};
@@ -163,10 +195,16 @@ function resizeBar() {
 }
 
 function genBarChart() {
+  var intervalLength = 800;
   var data = barData;
   var data2 = barData;
   $("#graph").empty();
   var index = 0;
+  if(index > 5 || index < 13) {
+    intervalLength = 600;
+  } else {
+    intervalLength = 800;
+  }
   var max = data[0].Value;
   for (var i = 0; i < data.length; i++){
     if (data[i].Value > max) max = data[i].Value;
@@ -183,11 +221,14 @@ function genBarChart() {
     var height = data[index].Value * scale;
     var top = 400 - height;
     var abbreviation = "";
+    var title = data[index].Key.replace(" ", "_");
     var split = data[index].Key.split(" ");
-    for (var q = 0; q < split.length; q++) abbreviation += split[q].charAt(0);
+    for (var q = 0; q < split.length; q++) {
+      abbreviation += split[q].charAt(0);
+    }
     $( "#ghost" ).append( "<div class='lines' style='width: 2 px; margin-top: -180px;  background-color:#BDBDBD; border:1px solid black'></div>" );  
    
-    $( "#graph" ).append( "<div class='bar' style='width:"+width+"px; margin-top:"+top+"px; background-color:#BDBDBD; border:1px solid black'>" + abbreviation + "</div>" );
+    $( "#graph" ).append( "<div class='bar' style='width:"+width+"px; margin-top:"+top+"px; background-color:#BDBDBD; border:1px solid black' title="+title+">" + abbreviation + "</div>" );
     $(".bar:nth-child("+(index+2)+")").animate({ height: "" + height + "px"}, 800);
      $("tr:nth-child("+(index+2)+") .barCount").css('font-weight', 'bold');
     
@@ -199,7 +240,7 @@ function genBarChart() {
 		clearInterval(intervalID2);
 	}
     
-  }, 80);
+  }, intervalLength);
 }
 
 function readLineData() {
@@ -219,6 +260,7 @@ function readLineData() {
 function clearLine() {
   $("table").empty();
   $("#graph").empty();
+  undoResize();
 }
 
 function genLineChart() {
@@ -285,18 +327,20 @@ function readPieData() {
 }
 
 function clearPie() {
+  index = 0;
+  lastAngle = 0;
   $("table").empty();
   $("#graph").empty();
-  //clear();
-  //setup();
 }
 
 function genPieChart() {
+  $("#graph").empty();
+  index = 0;
+  lastAngle = 0;
   var myCanvas = createCanvas(400, 400);
   myCanvas.parent('graph');
   noStroke();
-  ready = 0;
-  loop();
+  drawPiePieces();
 }
 
 function setup() {
@@ -304,15 +348,7 @@ function setup() {
 }
 
 function draw() {
-  if(ready > 0) {
-    drawPieces();
-    noLoop();
-  } else {
-    var myCanvas = createCanvas(400, 400);
-    myCanvas.parent('graph');
-    noStroke();
-    ready = 1;
-  }
+  
 }
 
 function drawLines(){
@@ -360,11 +396,11 @@ function createLine(x1,y1, x2,y2){
   return line;
 }
 
-function drawPieces() {
-  timeoutID = window.setInterval(drawPiece, 500);
+function drawPiePieces() {
+  timeoutID = window.setInterval(drawPiePiece, 500);
 }
 
-function drawPiece() {
+function drawPiePiece() {
   var data = pieData;
   $('tr:nth-child('+(index+1)+') .barCount').css('font-weight', '');
   $('tr:nth-child('+(index+2)+') .barCount').css('font-weight', 'bold');
@@ -372,29 +408,102 @@ function drawPiece() {
   i = index;
   var rand = Math.random();
   var color = rand*255;
-  var gray = map(i, 0, angles.length, 0, 255);
+  var gray = map(i, 0, pieAngles.length, 0, 255);
   //fill(gray);
   fill(gray, gray, 100);
-  arc(width/2, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
-  console.log( lastAngle+radians(angles[i]));
+  arc(width/2, height/2, diameter, diameter, lastAngle, lastAngle+radians(pieAngles[i]));
+  console.log( lastAngle+radians(pieAngles[i]));
   var adjustx;
   var adjusty;
-  if (lastAngle+radians(angles[i]) > (Math.PI/2) && lastAngle+radians(angles[i]) < (3*Math.PI/2)) {
-    adjustx = sin((lastAngle+radians(angles[i]))/2) * 50;
-  } else if (lastAngle+radians(angles[i]) > (3*Math.PI/2) && lastAngle+radians(angles[i]) < (2*Math.PI)) {
-    adjustx = -1 * cos((lastAngle+radians(angles[i]))) * 20;
+  if (lastAngle+radians(pieAngles[i]) > (Math.PI/2) && lastAngle+radians(pieAngles[i]) < (3*Math.PI/2)) {
+    adjustx = sin((lastAngle+radians(pieAngles[i]))/2) * 50;
+  } else if (lastAngle+radians(pieAngles[i]) > (3*Math.PI/2) && lastAngle+radians(pieAngles[i]) < (2*Math.PI)) {
+    adjustx = -1 * cos((lastAngle+radians(pieAngles[i]))) * 20;
   } else {
     adjustx = 0
   }
-  if (lastAngle+radians(angles[i]) > (Math.PI) && lastAngle+radians(angles[i]) < (2*Math.PI)) {
-    adjusty = sin((lastAngle+radians(angles[i]))) * 50;
+  if (lastAngle+radians(pieAngles[i]) > (Math.PI) && lastAngle+radians(pieAngles[i]) < (2*Math.PI)) {
+    adjusty = sin((lastAngle+radians(pieAngles[i]))) * 50;
   } else {
     adjusty = 0;
   }
-  text(data[i].Key + ", " + (angles[i]/360*100).toFixed(2) + "%", width/2 + cos ((lastAngle+.5*radians(angles[i])) )* (diameter / 2) - adjustx, height/2 +(sin (lastAngle+.5*radians(angles[i]))) * (diameter / 2) + adjusty, 50 , 50);
-  lastAngle += radians(angles[i]); 
+  text(data[i].Key + ", " + (pieAngles[i]/360*100).toFixed(2) + "%", width/2 + cos ((lastAngle+.5*radians(pieAngles[i])) )* (diameter / 2) - adjustx, height/2 +(sin (lastAngle+.5*radians(pieAngles[i]))) * (diameter / 2) + adjusty, 50 , 50);
+  lastAngle += radians(pieAngles[i]); 
   index++;
   if(index==data.length){
     clearInterval(timeoutID);
   }
+}
+
+function readDonutData() {
+  var data = donutData;
+  $("table").empty();
+  $( "table" ).append( "<th>Grade Distribution of Operating Systems</th>" );
+  $( "table" ).append( "<tr><td>" + "<u>Day</u>" + "</td><td>" + "<u>Number of Grades</u>" + "</td></tr>" );
+  data.forEach( function(element, index, array) {
+    //if(index == 1){alert(element.Key)};
+    if(index <= 17){
+      var string = "<tr><td>" + element.Key + "</td><td class='barCount'>" + element.Value + "</td></tr>";
+      $( "table" ).append(string);
+
+    };
+  });
+}
+
+function genDonutChart() {
+  $("#graph").empty();
+  index = 0;
+  lastAngle = 0;
+  var myCanvas = createCanvas(400, 400);
+  myCanvas.parent('graph');
+  noStroke();
+  drawDonutPieces();
+}
+
+function drawDonutPieces() {
+  timeoutID = window.setInterval(drawDonutPiece, 500);
+}
+
+function drawDonutPiece() {
+  var data = donutData;
+  $('tr:nth-child('+(index+1)+') .barCount').css('font-weight', '');
+  $('tr:nth-child('+(index+2)+') .barCount').css('font-weight', 'bold');
+  diameter = 300;
+  i = index;
+  var rand = Math.random();
+  var color = rand*255;
+  var gray = map(i, 0, donutAngles.length, 0, 255);
+  //fill(gray);
+  fill(gray + 120, gray, 50);
+  arc(width/2, height/2, diameter, diameter, lastAngle, lastAngle+radians(donutAngles[i]));
+  var adjustx;
+  var adjusty;
+  if (lastAngle+radians(donutAngles[i]) > (Math.PI/2) && lastAngle+radians(donutAngles[i]) < (3*Math.PI/2)) {
+    adjustx = sin((lastAngle+radians(donutAngles[i]))/2) * 50;
+  } else if (lastAngle+radians(donutAngles[i]) > (3*Math.PI/2) && lastAngle+radians(donutAngles[i]) < (2*Math.PI)) {
+    adjustx = -1 * cos((lastAngle+radians(donutAngles[i]))) * 20;
+  } else {
+    adjustx = 0
+  }
+  if (lastAngle+radians(donutAngles[i]) > (Math.PI) && lastAngle+radians(donutAngles[i]) < (2*Math.PI)) {
+    adjusty = sin((lastAngle+radians(donutAngles[i]))) * 50;
+  } else {
+    adjusty = 0;
+  }
+  fill(0);
+  text(data[i].Key + ", " + (donutAngles[i]/360*100).toFixed(2) + "%", width/2 + cos ((lastAngle+.5*radians(donutAngles[i])) )* (diameter / 2) - adjustx, height/2 +(sin (lastAngle+.5*radians(donutAngles[i]))) * (diameter / 2) + adjusty, 50 , 50);
+  lastAngle += radians(donutAngles[i]); 
+  index++;
+  if(index==data.length){
+    clearInterval(timeoutID);
+  }
+  fill(255);
+  ellipse(width/2, height/2, diameter / 2, diameter / 2);
+}
+
+function clearDonut() {
+  index = 0;
+  lastAngle = 0;
+  $("table").empty();
+  $("#graph").empty();
 }
